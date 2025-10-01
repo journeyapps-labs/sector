542f787466a6f70f4cf89162c22b81fec0e1f00e
Dylan Vorster <dylanvorster.dv@gmail.com>
2025-10-01 16:57:33 -0600

    export more classes

--- modules/module-data-browser/src/index.ts
+++ modules/module-data-browser/src/index.ts
@@ -10,18 +10,24 @@ export * from './core/query/AbstractQuery';
 export * from './core/query/Page';
 export * from './core/types/ManualConnectionFactory';
 export * from './core/types/ManualConnection';
+
 export * from './entities/QueryEntityDefinition';
 export * from './entities/ConnectionEntityDefinition';
 export * from './entities/ConnectionFactoryEntityDefinition';
 export * from './entities/SchemaModelDefinitionEntityDefinition';
 export * from './entities/SchemaModelObjectEntityDefinition';
+
 export * from './panels/query/QueryPanelFactory';
 export * from './panels/model/ModelPanelFactory';
+export * from './panels/_shared/SharedModelPanelFactory';
+
 export * from './stores/ConnectionStore';
+
 export * from './actions/connections/AddConnectionAction';
 export * from './actions/connections/RemoveConnectionAction';
 export * from './actions/schema-definitions/CreateModelAction';
 export * from './actions/schema-definitions/QuerySchemaModelAction';
+export * from './actions/schema-model/EditSchemaModelAction';
 export * from './actions/schema-model/EditSchemaModelAction';
 
 export default DataBrowserModule;
