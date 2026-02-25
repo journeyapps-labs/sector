576c27c2ae2ae2d670e145a6a84819ae1b807c35
Dylan Vorster <dylanvorster.dv@gmail.com>
2026-02-24 21:34:47 -0700

    the code has been cleaned up

--- modules/module-data-browser/src/core/query/query-simple/SimpleQueryFilterState.ts
+++ modules/module-data-browser/src/core/query/query-simple/SimpleQueryFilterState.ts
@@ -26,35 +26,32 @@ export class SimpleQueryFilterState extends BaseObserver<SimpleQueryFilterStateL
     this.typeEngine = options.typeEngine;
   }
 
+  get filters(): SimpleFilter[] {
+    return Array.from(this.simpleFilters.values());
+  }
+
+  clear() {
+    this.filters.forEach((filter) => filter.delete());
+  }
+
   setDefinition(definition: SchemaModelDefinition | undefined) {
     const definitionChanged = this.definition?.definition?.name !== definition?.definition?.name;
     this.definition = definition;
     const hadFilters = this.simpleFilters.size > 0;
     if (definitionChanged) {
-      Array.from(this.simpleFilters.values()).forEach((filter) => filter.delete());
-      this.simpleFilters.clear();
+      this.clear();
     }
-    if (definitionChanged || hadFilters) {
+    if (definitionChanged && !hadFilters) {
       this.iterateListeners((cb) => cb.changed?.());
     }
     return definitionChanged || hadFilters;
   }
 
   getFilterableFields(): { key: string; label: string }[] {
-    const definition = this.definition;
-    if (!definition?.definition) {
+    if (!this.definition?.definition) {
       return [];
     }
-    return _.map(definition.definition.attributes, (attribute) => {
-      const handler = this.typeEngine.getHandler(attribute.type);
-      if (!handler?.setupFilter) {
-        return null;
-      }
-      return {
-        key: attribute.name,
-        label: attribute.label || attribute.name
-      };
-    }).filter((v) => !!v);
+    return this.definition.getFilterableFields(this.typeEngine);
   }
 
   getActiveFilters(): { key: string; label: string; filter: SimpleFilter }[] {
@@ -69,29 +66,19 @@ export class SimpleQueryFilterState extends BaseObserver<SimpleQueryFilterStateL
 
   getFilter(field: string): SimpleFilter | undefined {
     const variable = this.resolveAttribute(field);
-    if (!variable) {
-      return undefined;
-    }
-    return this.simpleFilters.get(variable);
+    return variable ? this.simpleFilters.get(variable) : undefined;
   }
 
   getSerializedFilters() {
-    return Array.from(this.simpleFilters.values()).map((filter) => filter.serialize());
+    return this.filters.map((filter) => filter.serialize());
   }
 
   hydrateFilters(filters: SerializedSimpleFilter[]) {
+    this.clear();
     const definition = this.definition;
     if (!definition?.definition) {
-      const changed = this.simpleFilters.size > 0;
-      this.simpleFilters.clear();
-      if (changed) {
-        this.iterateListeners((cb) => cb.changed?.());
-      }
       return;
     }
-    let changed = this.simpleFilters.size > 0;
-    Array.from(this.simpleFilters.values()).forEach((filter) => filter.delete());
-    this.simpleFilters.clear();
     (filters || []).forEach((filter) => {
       if (!SimpleFilter.canDeserialize(filter)) {
         return;
@@ -103,11 +90,7 @@ export class SimpleQueryFilterState extends BaseObserver<SimpleQueryFilterStateL
         return;
       }
       this.addFilter(variable, SimpleFilter.deserialize(variable, filter));
-      changed = true;
     });
-    if (changed) {
-      this.iterateListeners((cb) => cb.changed?.());
-    }
   }
 
   setFilter(field: string, filter: SimpleFilter) {
@@ -140,9 +123,6 @@ export class SimpleQueryFilterState extends BaseObserver<SimpleQueryFilterStateL
   }
 
   private addFilter(variable: Variable, filter: SimpleFilter): boolean {
-    if (!variable || !filter) {
-      return false;
-    }
     const existing = this.simpleFilters.get(variable);
     if (existing && existing !== filter) {
       existing.delete();
@@ -166,11 +146,10 @@ export class SimpleQueryFilterState extends BaseObserver<SimpleQueryFilterStateL
   }
 
   private resolveAttribute(field: string): Variable | undefined {
-    const definition = this.definition;
-    if (!definition?.definition) {
+    if (!this.definition?.definition) {
       return undefined;
     }
-    return _.find(_.values(definition.definition.attributes), (attribute) => {
+    return _.find(_.values(this.definition.definition.attributes), (attribute) => {
       return attribute.name === field;
     });
   }
